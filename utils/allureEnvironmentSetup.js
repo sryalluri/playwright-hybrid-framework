20cbe4a0597632dfbdeee615bc636a51cd6a78cd
sryalluri <sr.yalluri@gmail.com>
2026-05-03 18:38:41 +0530

    updated yml file

--- utils/allureEnvironmentSetup.js
+++ utils/allureEnvironmentSetup.js
@@ -2,20 +2,20 @@ import fs from "fs";
 
 async function globalSetup() {
 
-  const resultsDir = "allure-results";
+    const resultsDir = "allure-results";
 
-  if (!fs.existsSync(resultsDir)) {
-    fs.mkdirSync(resultsDir);
-  }
+    if (!fs.existsSync(resultsDir)) {
+        fs.mkdirSync(resultsDir);
+    }
 
-  const content = `
+    const content = `
 Browser=Chromium
 Environment=${process.env.TEST_ENV || "QA"}
 BaseURL=${process.env.API_BASE_URL}
 Framework=Playwright
 `;
 
-  fs.writeFileSync(`${resultsDir}/environment.properties`, content);
+    fs.writeFileSync(`${resultsDir}/environment.properties`, content);
 }
 
 export default globalSetup;
